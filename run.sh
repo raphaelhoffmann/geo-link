021ee0c1d9698f8c8f5a2904a2f67906c53ff060
Xiao Ling <xiaoling.sh@gmail.com>
2015-11-03 17:31:51 -0800

    change run.sh to deepdive commands

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+read -p "WARNING: Script will erase sentences in database. Are you sure? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  exit
+fi
 
-DIRNAME=`dirname $0`
-
-. "${DIRNAME}/env_local.sh"
-
-cd $DEEPDIVE_HOME
-export PYTHONPATH=$DEEPDIVE_HOME/ddlib:$PYTHONPATH
-
-### Compile and run:
-sbt/sbt "run -c $APP_HOME/${APP_CONF:-application.conf} -o ${TMP_DIR}"
+dropdb geo
+deepdive initdb
+deepdive run
